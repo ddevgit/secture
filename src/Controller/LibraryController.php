2565ff3ad1da9a43ba27820fb844bfd1f8b1b192
divergentdev <divergentdev@tutanota.com>
2021-11-24 14:35:55 +0100

    create our first endpoint json library

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LibraryController extends AbstractController
+{
+    /**
+     * @Route("/library/list", name="library_list")
+     */
+    public function list()
+    {
+        $response = new JsonResponse();
+        $response->setData([
+           'success' =>true,
+           'data'  =>
+               [
+                'id' => 1,
+                'title' => 'Hacia rutas salvajes'
+                ]
+        ]);
+
+        return $response;
+    }
+
+}
