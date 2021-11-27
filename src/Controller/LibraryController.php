e4a77a055f1ae4319566016e552006337f2e36f2
divergentdev <divergentdev@tutanota.com>
2021-11-27 18:27:09 +0100

    add cruds

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -5,17 +5,30 @@ namespace App\Controller;
 
 
 
+use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
+    private  $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/library/list", name="library_list")
      */
-    public function list()
+    public function list(Request $request)
     {
+        $title = $request->get('title', '*******  // without title // ******');
+        $this->logger->warning($title);
         $response = new JsonResponse();
         $response->setData([
            'success' =>true,
@@ -23,6 +36,37 @@ class LibraryController extends AbstractController
                [
                 'id' => 1,
                 'title' => 'Hacia rutas salvajes'
+                ],
+                [
+                'id' => 2,
+                'title' => $title
+                ]
+        ]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/book/create", name="create_book")
+     */
+    public function createBook(Request $request, EntityManagerInterface $entityManager)
+    {
+        if(null != $request->get('title')){
+
+        }
+
+        $book = new Book();
+        $book->setTitle('hacia rutas salvajes');
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        $response = new JsonResponse();
+        $response->setData([
+            'success' =>true,
+            'data'  =>
+                [
+                    'id' => $book->getId(),
+                    'title' => $book->getTitle()
                 ]
         ]);
 
---
+++ src/Entity/Category.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Product", inversedBy="category")
+     * @ORM\JoinColumn(name="product_id", referencedColumnName="id")
+     */
+    private $product;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+}
