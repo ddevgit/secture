fa7283d0b8828c47ea69b5e9e92d41ea5e0635ad
divergentdev <divergentdev@tutanota.com>
2021-11-30 11:46:12 +0100

    add funtions to call api

--- src/Service/ConversionCurrencyService.php
+++ src/Service/ConversionCurrencyService.php
@@ -3,30 +3,37 @@
 
 namespace App\Service;
 
-use Doctrine\ORM\EntityManager;
-
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ConversionCurrencyService
 {
-    private $em;
+    protected $apiKey = "e532f4b799fec52e1107d185599c3e59";
+    protected $base_url = "https://api.exchangeratesapi.io/v1/";
+    protected $url_api = "https://api.exchangeratesapi.io/v1/latest
+    ? access_key = ". "e532f4b799fec52e1107d185599c3e59";
 
-    private $apiKey = "e532f4b799fec52e1107d185599c3e59";
-
-    /**
-     * HomeQueryManager constructor.
-     * @param $em
-     */
-    public function __construct(EntityManager $em)
+    public function __construct(HttpClientInterface $client)
     {
-        $this->em = $em;
+        $this->client = $client;
     }
 
-    public function callApi()
+    public function get_authorisation_code()
     {
+        $response = $this->client->request(
+            'GET',
+            $this->authorisation_url,
+            [
+                'query' => [
+                    'response_type' => 'code',
+                    'redirect_uri' => $this->redirect_uri,
+                    'client_id' => $this->client_id,
+                ]
+            ]
+        );
+
+        return $response->getContent();
 
 
     }
-
-
 
 }
